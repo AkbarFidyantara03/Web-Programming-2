25b9b9b40d45e5dff3c87471f14b153e99368657
AkbarFidyantara03 <62188181+AkbarFidyantara03@users.noreply.github.com>
2020-11-23 14:08:47 +0700

    Add files via upload

---
+++ Pertemuan 1/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan</h1>";
+		echo "<h3>Nama Saya Muhammad Akbar Fidyantara</h3>";
+		echo "<p>Saya tinggal di Dukuwaru</p>";
+		echo "Hobi saya Bermain Game Online";
+	}
+}
